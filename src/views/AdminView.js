accc513a039379a6c1eaa7fe5a413c8eabc37ea0
Nate Widmer <nate.widmer@noseryoung.ch>
2022-01-25 10:39:51 +0100

    Added server config

--- src/views/AdminView.js
+++ src/views/AdminView.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
 
 const AdminView=()=> {
+    const [animalsList, setAnimalList] = useState([]);
+
+    useEffect(() => {
+        Axios.get("http://localhost:3002/api/get").then((res)=>{
+            setAnimalList(res.data);
+        });
+    }, []);
+
+    console.log(animalsList);
     return (
         <h1>Hi Admin</h1>
     );
