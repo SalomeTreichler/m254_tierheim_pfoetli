afafc549c99646591ccb27dc5c824849d65d58d7
Salome Treichler <salome.treichler@accenture.com>
2022-01-25 09:37:16 +0100

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import AdminView from "./views/AdminView";
 import VisitorView from "./views/VisitorView";
-import {Link, Route, Router, Switch} from "react-router-dom";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/admin">Ich bin ein Admin</Link>
-              </li>
-              <li>
-                <Link to="/visitor">Ich bin ein Besucher</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/admin">
-              <AdminView />
-            </Route>
-            <Route path="/visitor">
-              <VisitorView />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-  );
+    return (
+        <Fragment>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/admin" element={<AdminView/>}/>
+                    <Route path="/visitors" element={<VisitorView/>}/>
+                </Routes>
+            </BrowserRouter>
+        </Fragment>
+    );
 }
 
 
