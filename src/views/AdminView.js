1c9b493a7f22808ba09884e6127483cb27418ce8
Salome Treichler <salome.treichler@accenture.com>
2022-01-25 11:38:18 +0100

    basic structure for admin page

--- src/views/AdminView.js
+++ src/views/AdminView.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {Button, Grid} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        backgroundColor:'#333b2e',
+        color: '#99B687',
+        borderRadius: '0px',
+        marginBottom: '25px'
+    },
+    container: {
+        padding: '25px',
+    },
+}));
 
 const AdminView=()=> {
+    const classes = useStyles();
     return (
-        <h1>Hi Admin</h1>
+        <Fragment>
+            <AppBar position="static" style={{backgroundColor:'#99B687'}}>
+                <Toolbar>
+                    <Typography variant="h5" style={{color: 'black'}}>
+                        Tierheim Administration
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        <Grid container className={classes.container}>
+            <Grid item xs={12}>
+                <Button variant={"contained"} className={classes.button}>
+                    Tier Erfassen
+                </Button>
+            </Grid>
+        </Grid>
+        </Fragment>
     );
 }
 
